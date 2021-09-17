dfab4d8a2f8b0bed43830b3f921dca14200a69cf
1992cma <1992cma@gmail.com>
2021-09-17 14:07:01 -0500

    Created the metatable from my database.

---
+++ StoreFront.DATA.EF/Metadata/StoreFrontMetadata.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;//added for metadata and validation
+
+namespace StoreFront.DATA.EF//.Metadata - name must match namespace of classes these partial classes will buddy up with.
+{
+    public class GameMetadata
+    {
+
+        [Required]
+        [StringLength(100)]
+        public string GameTitle { get; set; }
+
+        [Required]
+        public Nullable<int> PlatformID { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public Nullable<decimal> Price { get; set; }
+
+        [Required]
+        public Nullable<int> UnitsSold { get; set; }
+
+        [Required]
+        [Display(Name = "Released")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public Nullable<System.DateTime> ReleaseDate { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Department")]
+        public string DeptName { get; set; }
+
+        [Display(Name = "Company")]
+        public string Name { get; set; }
+
+    }
+}
